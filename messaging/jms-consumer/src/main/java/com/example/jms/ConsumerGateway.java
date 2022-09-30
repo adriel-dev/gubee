d5de515d1af9612a83ba0136d840b69a29d1b445
Adriel Felix <adrielandrade3@gmail.com>
2022-09-30 08:54:45 -0300

    consumer rodando com thread

--- messaging/jms-consumer/src/main/java/com/example/jms/ConsumerGateway.java
+++ messaging/jms-consumer/src/main/java/com/example/jms/ConsumerGateway.java
@@ -3,8 +3,11 @@ package com.example.jms;
 import com.example.port.in.UserMessageCommand;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSConsumer;
@@ -13,23 +16,26 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 @ApplicationScoped
-public class ConsumerGateway implements Gateway {
+public class ConsumerGateway implements Gateway, Runnable {
 
     @Inject
     ConnectionFactory connectionFactory;
 
-    private final List<UserMessageCommand> users = new ArrayList<>();
+    private final ExecutorService scheduler = Executors.newSingleThreadExecutor();
+
+    private List<UserMessageCommand> users = new ArrayList<>();
 
     @Override
     public List<UserMessageCommand> getMessages() {
-        getMessageFromQueue();
         return users;
     }
 
-    private void getMessageFromQueue() {
+    private void getMessagesFromQueue() {
         try (JMSContext context = connectionFactory.createContext(JMSContext.CLIENT_ACKNOWLEDGE)) {
             JMSConsumer consumer = context.createConsumer(context.createQueue("users"));
             while (true) {
@@ -46,6 +52,19 @@ public class ConsumerGateway implements Gateway {
         } catch (JMSException | JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    void onStart(@Observes StartupEvent ev) {
+        scheduler.submit(this);
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {
+        scheduler.shutdown();
+    }
+
+    @Override
+    public void run() {
+        getMessagesFromQueue();
     }
 
 }
